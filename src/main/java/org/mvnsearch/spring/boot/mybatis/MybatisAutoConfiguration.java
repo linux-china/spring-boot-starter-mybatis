17aae2eccfcc56256ca717488792066bb996ffbb
linux_china <linux_china@hotmail.com>
2015-12-19 14:31:15 +0800

    mybatis auto configuration

---
+++ src/main/java/org/mvnsearch/spring/boot/mybatis/MybatisAutoConfiguration.java
@@ -0,0 +1,105 @@
+/*
+ *    Copyright 2010-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.mvnsearch.spring.boot.mybatis;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+/**
+ * {@link EnableAutoConfiguration Auto-Configuration} for Mybatis. Contributes a
+ * {@link SqlSessionFactory} and a {@link SqlSessionTemplate}.
+ * <p>
+ * If {@link org.mybatis.spring.annotation.MapperScan} is used, or a configuration file is
+ * specified as a property, those will be considered, otherwise this auto-configuration
+ * will attempt to register mappers based on the interface definitions in or under the
+ * root auto-configuration package.
+ *
+ * @author Eddú Meléndez
+ * @author Josh Long
+ * @author linux_china
+ */
+@SuppressWarnings("SpringJavaAutowiringInspection")
+@Configuration
+@ConditionalOnClass({ SqlSessionFactory.class, SqlSessionFactoryBean.class })
+@ConditionalOnBean(DataSource.class)
+@EnableConfigurationProperties(MybatisProperties.class)
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+public class MybatisAutoConfiguration {
+    private static Log log = LogFactory.getLog(MybatisAutoConfiguration.class);
+    @Autowired
+    private MybatisProperties properties;
+    @Autowired
+    private ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+    @PostConstruct
+    public void checkConfigFileExists() {
+        Assert.state(properties.getConfig() != null, "Cannot find config location:  (please add config file or check your Mybatis configuration)");
+        if (this.properties.getConfig() != null) {
+            Resource resource = this.resourceLoader.getResource(this.properties.getConfig());
+            Assert.state(resource.exists(), "Cannot find config location: " + resource
+                    + " (please add config file or check your Mybatis configuration)");
+        }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(SqlSessionFactory.class)
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setConfigLocation(this.resourceLoader.getResource(this.properties.getConfig()));
+        return factory.getObject();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public MetricsInterceptor mybatisMetricsInterceptor(SqlSessionFactory sqlSessionFactory) {
+        MetricsInterceptor interceptor = new MetricsInterceptor();
+        sqlSessionFactory.getConfiguration().addInterceptor(interceptor);
+        return interceptor;
+    }
+
+    @Bean
+    public MybatisEndpoint mybatisEndpoint(MybatisProperties properties, SqlSessionFactory sqlSessionFactory, MetricsInterceptor interceptor) {
+        return new MybatisEndpoint(properties, sqlSessionFactory, interceptor);
+    }
+
+}
